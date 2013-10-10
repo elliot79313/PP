81a9a449014cac6f76313e389a0e6de93e76d826
QSearch <elliot79313@gmail.com>
2013-10-10 20:56:55 +0800

    Version 1.1

--- hw1_15.py
+++ hw1_15.py
@@ -5,20 +5,22 @@ for line in f:
     data.append(map(float, line.split()))
 
 
-w = [0,0,0,0]
+w = [0,0,0,0,0]
 
 count = 0
 
 while True:
     correct_num = 0
     for i, e in enumerate(data):
-        dot = (w[0] * e[0] + w[1] * e[1] + w[2] * e[2] + w[3] * e[3])
+        dot = (w[0] * e[0] + w[1] * e[1] + w[2] * e[2] + w[3] * e[3] + w[4] * 1)
+
         if  dot * e[4] <=0:
             #correct
             w[0] = w[0] + e[0] * e[4]
             w[1] = w[1] + e[1] * e[4]
             w[2] = w[2] + e[2] * e[4]
             w[3] = w[3] + e[3] * e[4]
+            w[4] = w[4] + 1 * e[4]
 
             count = count + 1
 
